156fe3bde904d5f919b0134b68df32700e51c04f
Robin Karlsson <rk222ev@student.lnu.se>
2015-09-01 15:46:56 +0200

    Added ruby implementation.

---
+++ DiceGame/DiceGame_ruby/Dice.rb
@@ -0,0 +1,14 @@
+class Dice
+  # Readable attribute
+  attr_reader :face_value
+
+  # Constructor
+  def initialize
+    @face_value = 0
+  end
+
+  # Roll the Die
+  def roll
+    @face_value = rand 6
+  end
+end
