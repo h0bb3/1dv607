427d3dace4521c748e7e972ef1ffa651ca26bb96
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-09-04 09:45:10 +0200

    Ported app to TypeScript
    Uses HTML-elements to display result, could just have used console.log()
    instead.

---
+++ DiceGame/DiceGame_typescript/app.ts
@@ -0,0 +1,27 @@
+﻿module DiceGame_TS {
+    export class app {
+        element: HTMLElement;
+        span: HTMLElement;
+
+        game: DiceGame = new DiceGame();
+
+        constructor(element: HTMLElement) {
+            this.element = element;
+            this.span = document.createElement('span');
+            this.element.appendChild(this.span);
+        }
+
+        start() {
+            if (this.game.Play())
+                this.span.innerHTML = "You are a winner";
+            else
+                this.span.innerHTML = "Sorry";
+        }
+    }
+}
+
+window.onload = () => {
+    var element = document.getElementById('content');
+    var game = new DiceGame_TS.app(element);
+    game.start();
+};
