e826c2bce8701130a8fe165ca5615ad02e3aa59d
Robin Karlsson <rk222ev@student.lnu.se>
2015-09-16 20:59:00 +0200

    Adds ruby MVC

---
+++ MVC_DiceGame/MVC_DiceGame_ruby/view/Console.rb
@@ -0,0 +1,25 @@
+module View
+  class Console
+
+    QUIT_ON_INPUT  = "q"
+
+    def display_instructions
+      message = "Welcome to the cool Dice Game. Press any Key to play, or #{QUIT_ON_INPUT} to Quit"
+      system "clear"
+      puts message
+    end
+
+    def wants_to_quit?
+      gets == "#{QUIT_ON_INPUT}\n"
+    end
+
+    def display_result(player_won, dice_one_value, dice_two_value)
+        if player_won
+          puts "You are a winner: #{dice_one_value} #{dice_two_value}"
+        else
+          puts "Sorry you lost: #{dice_one_value} #{dice_two_value}"
+        end
+    end
+
+  end
+end
